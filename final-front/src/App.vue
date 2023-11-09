844eb3d8b229139ff4c5165e5a592555d8b34475
DavinChoi <wlwhstjdgur@naver.com>
2023-11-09 16:50:14 +0900

    view pages
    2

--- final-front/src/App.vue
+++ final-front/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import TheHeader from "./components/layout/TheHeader.vue";
 import TheFooter from "./components/layout/TheFooter.vue";
+import TheMain from "./views/TheMain.vue";
+import TheLogin from "./views/TheLogin.vue";
 </script>
 
 <template>
   <TheHeader></TheHeader>
-
+  <TheLogin></TheLogin>
+  <!-- <TheMain /> -->
   <!-- <RouterView></RouterView> -->
   <TheFooter></TheFooter>
 </template>
