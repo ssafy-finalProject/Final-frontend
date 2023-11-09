83ae470a85950b2dd565ce88ecb96834acb14179
DavinChoi <wlwhstjdgur@naver.com>
2023-11-09 17:59:12 +0900

    page add

--- final-front/src/App.vue
+++ final-front/src/App.vue
@@ -6,11 +6,17 @@ import TheLogin from "./components/member/TheLogin.vue";
 import TheFindPassWord from "./components/member/TheFindPassWord.vue";
 import TheJoin from "./components/member/TheJoin.vue";
 import TheMyPage from "./components/member/TheMyPage.vue";
+import TheBoardView from "./components/board/TheBoardView.vue";
+import TheBoardWrite from "./components/board/TheBoardWrite.vue";
+import TheBoardModify from "./components/board/TheBoardModify.vue";
 </script>
 
 <template>
   <TheHeader></TheHeader>
-  <TheMyPage></TheMyPage>
+  <TheBoardModify></TheBoardModify>
+  <!-- <TheBoardWrite></TheBoardWrite> -->
+  <!-- <TheBoardView /> -->
+  <!-- <TheMyPage></TheMyPage> -->
   <!-- <TheJoin></TheJoin> -->
   <!-- <TheFindPassWord></TheFindPassWord> -->
   <!-- <TheLogin></TheLogin> -->
---
+++ final-front/src/components/board/TheBoardView.vue
@@ -0,0 +1,95 @@
+<script setup>
+
+</script>
+
+<template>
+<c:if test="${article eq null}">
+		<!-- <script>
+		alert("글이 삭제되었거나 부적절한 URL 접근입니다.");
+		location.href = "${root}/article/list";
+		</script> -->
+	</c:if>
+      <div class="row justify-content-center">
+        <div class="col-lg-8 col-md-10 col-sm-12">
+          <h2 class="my-3 py-3 shadow-sm bg-light text-center">
+            <mark class="sky">글보기</mark>
+          </h2>
+        </div>
+        <div class="col-lg-8 col-md-10 col-sm-12">
+          <div class="row my-2">
+            <h2 class="text-secondary px-5">${article.article_no}. ${article.subject}</h2>
+          </div>
+          <div class="row">
+            <div class="col-md-8">
+              <div class="clearfix align-content-center">
+                <img
+                  class="avatar me-2 float-md-start bg-light p-2"
+                  src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg"
+                />
+                <p>
+                  <span class="fw-bold">${article.user_id}</span> <br />
+                  <span class="text-secondary fw-light"> ${article.register_time} 조회 : ${article.hit} </span>
+                </p>
+              </div>
+            </div>
+            <div class="col-md-4 align-self-center text-end">댓글 : 17</div>
+            <div class="divider mb-3"></div>
+            <div class="text-secondary">
+              ${article.content}
+            </div>
+            <div class="divider mt-3 mb-3"></div>
+            <div class="d-flex justify-content-end">
+              <button type="button" id="btn-list" class="btn btn-outline-primary mb-3">
+                글목록
+              </button>
+              <c:if test="${userInfo.userId eq article.user_id}">
+              <button type="button" id="btn-mv-modify" class="btn btn-outline-success mb-3 ms-1">
+                글수정
+              </button>
+              <button type="button" id="btn-delete" class="btn btn-outline-danger mb-3 ms-1">
+                글삭제
+              </button>
+              </c:if>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- 댓글 시작 -->
+    <div class="container mt-3">
+    <h2>댓글</h2>
+    	<c:forEach var="i" items="${comments}">
+    	<form action="${root }/article?action=deleteComment" method="POST">
+    	<div class="card">
+    	<input type="hidden" name="articleno" value=${article.article_no }>
+    	<input type="hidden" name="commentno" value=${i.commentId }>
+    	${i.commentId }
+		  <div class="card-header">아이디 : ${i.userId}</div>
+		  <div class="card-body">댓글 : ${i.commentText}</div>
+		      	
+		</div>
+    	<c:if test="${userInfo.userId eq i.userId}">
+              <button type="submit" id="comment-delete" class="btn btn-outline-danger mb-3 ms-1">
+                삭제
+              </button>
+        </c:if>
+        </form>
+    	</c:forEach>
+    </div>	
+	<!-- 댓글 끝 -->
+	<!-- 댓글 추가 -->
+    <div class="container mt-3">
+	  <form action="${root}/article" method = "POST">
+	  	<input type="hidden" name="action" value="addComment">
+         <input type="hidden" name="articleno" value="${article.article_no}">
+	    <div class="mb-3 mt-3">
+	      <label for="comment">댓글 달기:</label>
+	      <textarea class="form-control" rows="5" id="comment" name="comment" required></textarea>
+	    </div>
+	    <button type="submit" class="btn btn-primary">Submit</button>
+	  </form>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
