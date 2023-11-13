ef9c8125c62e4655d557ad453712abbd16a0cc80
DavinChoi <39028878+wlwhstjdgur@users.noreply.github.com>
2023-11-13 21:35:01 +0900

    boardwrite
    boardwrite

--- final-front/src/App.vue
+++ final-front/src/App.vue
@@ -5,13 +5,17 @@ import TheMain from "./views/TheMain.vue";
 import TheLogin from "./components/member/TheLogin.vue";
 import TheMap from "./components/map/TheMap.vue";
 import KakaoMap from "./views/KakaoMap.vue";
+import TheBoardWrite from "./components/board/TheBoardWrite.vue";
+import TheBoardView from "./components/board/TheBoardView.vue";
 </script>
 
 <template>
   <div>
     <TheHeader></TheHeader>
-    <TheMap></TheMap>
-    <KakaoMap></KakaoMap>
+    <TheBoardView/>
+    <!-- <TheBoardWrite/> -->
+    <!-- <TheMap></TheMap> -->
+    <!-- <KakaoMap></KakaoMap> -->
     <!-- <router-view></router-view> -->
     <!-- <TheFooter></TheFooter> -->
   </div>
