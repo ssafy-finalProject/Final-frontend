c889f41bf24e3e497cebbe95b01e8c2dda99b802
HYUNJUN, KIM <denny10002@naver.com>
2023-11-17 17:52:05 +0900

    travelList, kakaoMap v1 수정 필요
    travelList에서 삭제에 대한 내용을 이벤트 처리로 emit으로 올려보내야 함

--- final-front/src/views/KakaoMap.vue
+++ final-front/src/views/KakaoMap.vue
@@ -4,10 +4,56 @@ import TravelList from "../components/map/TravelList.vue";
 
 var map;
 var realData = ref([]);
+let polyline;
 const searchKeyword = ref("");
-const positions = ref([]);
-const markers = ref([]);
-const startPlace = ref("");
+
+const markers = ref([]); // 시작지
+const stopover = ref([]); // 경유지
+const destination = ref([]); // 도착지
+
+// 부모가 자식의 이벤트 처리에 대한 listen 처리
+const listenList = ({
+  markers: newMarkers,
+  stopover: newStopover,
+  destination: newDestination,
+}) => {
+  // polyline이 그려져 있으면 값을 비운다.
+  if (polyline) {
+    polyline.setMap(null);
+    console.log(polyline);
+  }
+  // 배열 비우기
+  markers.value = [...newMarkers];
+  stopover.value = [...newStopover];
+  destination.value = [...newDestination];
+  console.log(markers, stopover, destination);
+
+  var linePath = [];
+
+  linePath.push(new kakao.maps.LatLng(markers.value[0].y, markers.value[0].x));
+  for (let i = 0; i < stopover.value.length; i++) {
+    linePath.push(
+      new kakao.maps.LatLng(stopover.value[i].y, stopover.value[i].x)
+    );
+  }
+  linePath.push(
+    new kakao.maps.LatLng(destination.value[0].y, destination.value[0].x)
+  );
+  console.log("좌표 값들: " + linePath);
+  // 지도에 표시할 선을 생성합니다
+  polyline = new kakao.maps.Polyline({
+    path: linePath, // 선을 구성하는 좌표배열 입니다
+    strokeWeight: 5, // 선의 두께 입니다
+    strokeColor: "#FFAE00", // 선의 색깔입니다
+    strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: "solid", // 선의 스타일입니다
+  });
+  console.log(polyline);
+  // 지도에 선을 표시합니다
+  polyline.setMap(map);
+  // 선을 구성하는 좌표 배열입니다. 이 좌표들을 이어서 선을 표시합니다
+}; // 기존의 배열 값을 전부 비우고, emit 받은 값들로 전부 replace 한다.
+
 var ps;
 var infowindow;
 
@@ -59,7 +105,6 @@ const initMap = () => {
   infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
 };
 
-
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
 function placesSearchCB(data, status, pagination) {
   if (status === kakao.maps.services.Status.OK) {
@@ -80,14 +125,23 @@ function placesSearchCB(data, status, pagination) {
   }
 }
 
-function sendMarkerInfo(place) {
-  if (markers.value.length === 1) {
-    markers.value.pop();
-    // console.log(markers.value.length);
+function sendMarkerInfo(place, type) {
+  if (type === "출발지") {
+    if (markers.value.length === 1) {
+      markers.value.pop();
+    }
+    markers.value.push(place);
+    console.log(markers);
+  } else if (type === "경유지") {
+    stopover.value.push(place);
+    console.log(stopover);
+  } else {
+    if (destination.value.length === 1) {
+      destination.value.pop();
+    }
+    destination.value.push(place);
+    console.log(destination);
   }
-  markers.value.push(place);
-  // console.log(markers.value.length);
-  // console.log(markers);
 }
 
 // 지도에 마커를 표시하는 함수입니다
@@ -98,34 +152,49 @@ function displayMarker(place) {
     position: new kakao.maps.LatLng(place.y, place.x),
   });
 
-  // console.log(place);
-
-  
-
   // 마커에 클릭이벤트를 등록합니다
   kakao.maps.event.addListener(marker, "click", function () {
     // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-    infowindow.setContent(
-      '<div style="padding:5px;font-size:12px;text-align=center">' +
-        place.place_name +
-        "</div>" +
-        `<div><button id="infoBtn" onclick="sendMarkerInfoHandler(${place.id})">` +
-        "추가" +
-        "</div>"
-    );
+    infowindow.setContent(`
+      <div class="infowindow-container">
+        <div style="margin-bottom: 5px;">${place.place_name}</div>
+        <div class="infowindow-buttons">
+          <button id="infoBtn" onclick="sendMarkerInfoHandler(${place.id}, '출발지')">출발지</button>
+          <button id="midBtn" onclick="sendMarkerInfoHandler(${place.id}, '경유지')">경유지</button>
+          <button id="lastBtn" onclick="sendMarkerInfoHandler(${place.id}, '도착지')">도착지</button>
+        </div>
+      </div>
+    `);
 
     infowindow.open(map, marker);
   });
-  // 버튼 클릭 이벤트 핸들러 추가
-  window.sendMarkerInfoHandler = function (id) {
-    console.log(id);
-    for (let i = 0; i < realData.length; i++){
-      // console.log(realData[i].id);
-      if (realData[i].id == id) {
-        console.log("현재 찾은 도시이름 : " + realData[i].address_name);
+
+  // 클릭 이벤트 핸들러 추가 ( 시작지, 경유지, 도착지에 따른 값을 type으로 받고, 받은 type들을 통해서,
+  // 함수들의 배열값들을 달리 넣어준다. 시작지는 markers, 경유지는 stopover, 도착지는 destination에 넣는다.
+  window.sendMarkerInfoHandler = function (id, type) {
+    // console.log(`${type} 선택: ${type}`);
+    if (type === "출발지") {
+      for (let i = 0; i < realData.length; i++) {
+        if (realData[i].id == id) {
+          console.log("현재 찾은 도시이름: " + realData[i].address_name);
+          sendMarkerInfo(realData[i], type);
+        }
+      }
+    } else if (type === "경유지") {
+      for (let i = 0; i < realData.length; i++) {
+        if (realData[i].id == id) {
+          console.log("현재 찾은 도시이름: " + realData[i].address_name);
+          sendMarkerInfo(realData[i], type);
+        }
+      }
+    } else {
+      for (let i = 0; i < realData.length; i++) {
+        if (realData[i].id == id) {
+          console.log("현재 찾은 도시이름: " + realData[i].address_name);
+          sendMarkerInfo(realData[i], type);
+        }
       }
     }
-    sendMarkerInfo(place);
   };
 }
 </script>
@@ -137,7 +206,14 @@ function displayMarker(place) {
     </div>
     <div class="container-fluid row">
       <div id="map" class="col-8"></div>
-      <TravelList :markers="markers" class="col-4"/>
+      <TravelList
+        :markers="markers"
+        :stopover="stopover"
+        :destination="destination"
+        class="col-4"
+        @send-list="listenList"
+      />
+      <!--자식에서 부모에게 send-list라는 이벤트를 발생했는데, 그걸 listen을 통해서 듣고 잇다가, 발생하면 이제 함수 처리한다.-->
     </div>
   </div>
 </template>
@@ -155,5 +231,21 @@ function displayMarker(place) {
 
 #map {
   height: 700px;
+}
+.infowindow-container {
+  max-width: 300px; /* Adjust the maximum width as needed */
+  padding: 10px;
+  text-align: center;
+}
+
+.infowindow-buttons {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+}
+
+.infowindow-buttons button {
+  flex-grow: 1;
+  margin: 0 2px;
 }
 </style>
