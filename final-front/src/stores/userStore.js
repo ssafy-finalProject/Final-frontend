edaa43457c73214e6e393113eeacba3d7dfc6c32
DavinChoi <39028878+wlwhstjdgur@users.noreply.github.com>
2023-11-19 14:35:56 +0900

    test
    test

--- final-front/src/stores/userStore.js
+++ final-front/src/stores/userStore.js
@@ -6,19 +6,31 @@ export const useAuthStore = defineStore('auth', {
     password: '',
     isLoggedIn: false,
   }),
-  actions: {
-    login() {
-      // 여기에서 실제 로그인 로직을 구현합니다.
-      // 예를 들어, 사용자 이름과 비밀번호를 확인하고 로그인 상태를 설정할 수 있습니다.
-      if (this.username === 'example' && this.password === 'password') {
+  ctions: {
+    async login() {
+      try {
+        const response = await axios.post('/api/login', {
+          username: this.username,
+          password: this.password,
+        });
+
+        // 성공적으로 로그인했을 때 서버로부터 받은 데이터 또는 토큰을 처리합니다.
+        // 여기에서는 간단하게 isLoggedIn을 true로 설정합니다.
         this.isLoggedIn = true;
-      } else {
+      } catch (error) {
+        // 로그인에 실패한 경우 에러를 처리할 수 있습니다.
+        console.error('Login failed:', error);
         this.isLoggedIn = false;
       }
     },
-    logout() {
-      // 로그아웃 로직
-      this.isLoggedIn = false;
+    async logout() {
+      try {
+        // 서버에 로그아웃 요청을 보낼 수 있습니다.
+        // 여기에서는 단순히 클라이언트 상태만 변경합니다.
+        this.isLoggedIn = false;
+      } catch (error) {
+        console.error('Logout failed:', error);
+      }
     },
   },
 });
